639ef344e11b7de8ce516844baaa4038282fde77
MatthewASC6 <matthew.castillo2021@gmail.com>
2019-07-11 11:57:26 -0400

    Draft of Random

---
+++ randomness.js
@@ -0,0 +1,40 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = "abcdefghiklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+
+function randomLetter(){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    Math.random()[alphabet]
+    console.log(Math.random()[alphabet])
+}
+function randomFood(){
+
+}
+function randomStudent(){
+
+}
+function randomGroup(){
+
+}
+randomLetter();
